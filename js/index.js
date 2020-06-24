2fec55b4e6c6b6080f1240cef9c71b359de4333b
Mohammad-Eshtaiwi <mohd.eshtiwi@gmail.com>
2020-06-24 11:40:45 +0300

    improved my code with functions

--- js/index.js
+++ js/index.js
@@ -1,35 +1,39 @@
 "use strict";
+let points;
 
-let passed = confirm(
-  "do you love soraka ? \n press ok for yes \n press cancel for no"
-);
-// kick the user out if he didnt pass
-if (!passed) window.close();
+function IsWorthy() {
+  let worthy = confirm(
+    "do you love soraka ? \n press ok for yes \n press cancel for no"
+  );
+  // kick the user out if he didnt pass
+  if (!worthy) window.close();
+}
+function isMaster() {
+  points = prompt("how many points do you have with soraka", "0");
+  points = parseInt(points);
+  // if user have high points call him master else he is noob
+  if (points >= 50000)
+    document.querySelector(".banner-content h2 span").innerHTML = "Master";
+}
+function makeOffer() {
+  // give offer to the user if he is master of soraka soraka
+  let prices = [520, 1350];
+  prices = prices.map((price) => {
+    if (points >= 50000) {
+      return Math.floor(price / 2);
+    }
+    return price;
+  });
+  console.log(prices);
 
-let points = prompt("how many points do you have with soraka", "0");
+  const StarGuardianSkin = document.querySelector(
+    ".StarGuardianSkin .price span"
+  );
 
-// if user have high points call him master else he is noob
-
-if (points >= 50000)
-  document.querySelector(".banner-content h2 span").innerHTML = "Master";
-
-// give offer to the user if he is master of soraka soraka
-points = parseInt(points);
-let prices = [520, 1350];
-prices = prices.map((price) => {
-  if (points >= 50000) {
-    return Math.floor(price / 2);
-  }
-  return price;
-});
-console.log(prices);
-
-const StarGuardianSkin = document.querySelector(
-  ".StarGuardianSkin .price span"
-);
-
-const DryadSkin = document.querySelector(".DryadSkin .price span");
-DryadSkin.innerHTML = prices[0];
-StarGuardianSkin.innerHTML = prices[1];
-console.log(DryadSkin);
-console.log(StarGuardianSkin);
+  const DryadSkin = document.querySelector(".DryadSkin .price span");
+  DryadSkin.innerHTML = prices[0];
+  StarGuardianSkin.innerHTML = prices[1];
+}
+IsWorthy();
+isMaster();
+makeOffer();
